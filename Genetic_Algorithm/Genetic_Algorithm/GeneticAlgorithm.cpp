23f2f9208b2da75f8c00d9b08235e5f64a3d59b1
S I M O N. P <spau@hotmail.de>
2019-08-29 23:03:48 +0200

    Basic Variables and Functions

--- Genetic_Algorithm/Genetic_Algorithm/GeneticAlgorithm.cpp
+++ Genetic_Algorithm/Genetic_Algorithm/GeneticAlgorithm.cpp
@@ -1,12 +1,63 @@
 #include "GeneticAlgorithm.h"
 
+//ctor
+
+GeneticAlgorithm::GeneticAlgorithm(float& fTrgt): fTarget{fTrgt}
+{
+	__init__pop();
+}
 
 
-GeneticAlgorithm::GeneticAlgorithm()
+//dtor
+
+GeneticAlgorithm::~GeneticAlgorithm()
 {
 }
 
 
-GeneticAlgorithm::~GeneticAlgorithm()
-{
+//public member functions
+
+float GeneticAlgorithm::operator=(float& fnew_target) {
+	fTarget = fnew_target;
+}
+
+void GeneticAlgorithm::solve(GeneticAlgorithm* GA_ptr) {
+	__init__pop(); //initialize the population
+
+	generation_count = 0; //set generation counter back to 0;
+
+	while(!bFoundSolution){ //run through the algorithm untill we found a solution or we hit MAX_ALLOWED_GENERATIONS
+		ftotal_fitness = 0; //set the total fitness of the current generation back to 0
+
+	}
+}
+
+
+//private member functions
+
+void GeneticAlgorithm::__init__rand() {
+	srand((int)time(nullptr));
+}
+
+void GeneticAlgorithm::__init__pop() {  
+	__init__rand();
+	for (int i = 0; i < POP_SIZE; i++) {
+		v_population.at(i).bits = getRandomBits(CHROMO_LENGTH); //Create a random Chromosome
+		v_population.at(i).fFitness = .0f;
+	}
+}
+
+std::string GeneticAlgorithm::getRandomBits(int length) { 
+	std::string temp = "";
+	for (int i = 0; i < length; i++) {         //choose 1 or 0
+		if (RANDOM_NUM > 0.5f)       
+			temp += '1';
+		else
+			temp += '0';
+	}
+	return temp;
+}
+
+float& GeneticAlgorithm::AssigneFitness(float& fTrgt, float& tot_ftns) {
+
 }
--- Genetic_Algorithm/Genetic_Algorithm/GeneticAlgorithm.h
+++ Genetic_Algorithm/Genetic_Algorithm/GeneticAlgorithm.h
@@ -1,17 +1,59 @@
 #pragma once
-#include <iostream>
-#include <string>
-#include <ctime>
-#include <cstdlib>
-#include <cmath>
+#include <iostream> //Input and Output
+#include <string> //string
+#include <ctime>  //time to initialize rand()
+#include <cstdlib> //substr()
+#include <cmath> //rand()
+#include <vector> //we use a vector to hold the population
 
-using std::cout;
-using std::endl;
+using std::cout;            //for better readability
+using std::endl;            //for better readability
 
-class GeneticAlgorithm
+#define CROSSOVER_RATE 0.07 //Rate after which crossover will be applied
+#define MUTATION_RATE 0.001 //The chance the a bit gets flipped
+#define GENE_LENGTH 4 //The length of each gene 
+#define CHROMO_LENGTH 300 //The length of each chromosome generated
+#define POP_SIZE 300 //Size of the population. 
+#define MAX_ALLOWED_GENERATIONS 1000 //The algorithm will stop after reaching this number
+
+#define RANDOM_NUM ((float)rand()/(RAND_MAX +1))
+
+
+class GeneticAlgorithm      //Class to hold the genetic Algorithm
 {
+	float fTarget = .0f; //The target 
+	float ftotal_fitness = .0f; //the total fitness of the chromosomes to 
+
+	int generation_count = 0; //how many generations have we made
+	int solutions_pos = 0; //The index where we found our solution
+
+	bool bFoundSolution = false; //have we found a solution yet
+
+	struct s_chromo_type {   //A struct to hold the actual chromosome
+		std::string bits{ "" }; //Holds the encoded Chromosome
+		float fFitness = .0f; //Holds the fitness of this Chromosome
+
+		s_chromo_type() : bits{ "" }, fFitness{ .0f }{};
+		s_chromo_type(std::string bts, float ftns) : bits{ bts }, fFitness{ ftns }{};
+	};
+
+	std::vector<s_chromo_type> v_population{ POP_SIZE }; //this is our population 
+
+	void __init__rand(); //initialize rand() with the time
+	void __init__pop(); //initialize a new random population
+
+	std::string getRandomBits(int); //create a random chromosome
+
+	float& AssigneFitness(float&, float&);
+
 public:
-	GeneticAlgorithm();
+
+	GeneticAlgorithm() = delete;
+	GeneticAlgorithm(float&);
+
 	~GeneticAlgorithm();
+
+	float operator=(float&); //use '=' to set new target
+	void solve(GeneticAlgorithm*); //member function to find a way to solve our problem
 };
 
